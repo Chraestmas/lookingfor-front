f2334d245dfc7bbf7520e877ac4fcb7ccd6834f9
Jaewoo Chun <jaewoochun@Jaewoos-Laptop.local>
2024-11-10 18:26:53 +0900

    UploadDetailSection & UploadImageSection updated

--- src/components/section/UploadDetailSection.vue
+++ src/components/section/UploadDetailSection.vue
@@ -1,5 +1,43 @@
 <template>
-  <h1>윤서님 페이지</h1>
+  <h1></h1>
+  <div class="features-block"></div>
+  <section class="features-metrics">
+    <div class="container-4">
+      <div class="pricing-grid">
+        <div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac4f-e6b90512" class="pricing-card-three">
+          <h3>Mobile Device</h3>
+          <p class="pricing-card-text">Category<br></p>
+        </div>
+        <div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac55-e6b90512" class="pricing-card-three">
+          <h3>Secondary Library</h3>
+          <p class="pricing-card-text">Current Location<br>
+          </p>
+        </div>
+        <div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac5b-e6b90512" class="pricing-card-three">
+          <h3>N/A</h3>
+          <p class="pricing-card-text">Name Tag</p>
+        </div><div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac60-e6b90512" class="pricing-card-three">
+          <h3>2/2/2024</h3>
+          <p class="pricing-card-text">Date Found</p>
+        </div><div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac65-e6b90512" class="pricing-card-three">
+          <h3>Found By:&nbsp;Jake</h3>
+          <p class="pricing-card-text">Status &amp;&nbsp;Pickup person name</p>
+        </div>
+        <div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac6a-e6b90512" class="pricing-card-three">
+          <h3>2/3/2024</h3>
+          <p class="pricing-card-text">Date Retrieved</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="hero-without-image-2">
+    <div class="container-9">
+      <div class="hero-wrapper-two-4">
+        <h1>Description</h1>
+        <p class="margin-bottom-24px-5">Slightly cracked screen, jelly case w/ ornaments inside, out of battery when found</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
--- src/components/section/UploadImageSection.vue
+++ src/components/section/UploadImageSection.vue
@@ -1,9 +1,50 @@
 <template>
   <h1>재우님 페이지</h1>
+  <section class="hero-stack-2">
+    <div class="container-4">
+      <div class="hero-wrapper-two-2">
+        <h1 class="heading-6">Item Name</h1>
+      </div>
+    </div>
+    <div class="w-layout-blockcontainer container-7 w-container">
+      <div class="f-section-large-2">
+        <div class="f-container-regular-2">
+          <div class="f-margin-bottom-48">
+            <div class="f-title-wrapper-center">
+              <h3 class="f-h3-heading">Images</h3>
+            </div>
+          </div>
+          <div class="w-layout-grid f-gallery-three-column">
+            <a href="#" class="f-gallery-lightbox w-inline-block w-lightbox">
+              <div class="f-gallery-image-s"><img src="../../assets/Placeholder-Gallery.svg" loading="lazy" alt="" class="f-image-cover"></div>
+            </a>
+            <a href="#" id="w-node-_18ecd1bb-2568-58ec-8730-6addda8b1368-e6b90512" class="f-gallery-lightbox w-inline-block w-lightbox">
+              <div class="f-gallery-image-s"><img src="../../assets/Placeholder-Gallery.svg" loading="lazy" alt="" class="f-image-cover"></div>
+            </a>
+            <a href="#" id="w-node-_551e7c5d-5efd-50a2-c017-fcdb17c6e88e-e6b90512" class="f-gallery-lightbox w-inline-block w-lightbox">
+              <div class="f-gallery-image-s"><img src="../../assets/Placeholder-Gallery.svg" loading="lazy" alt="" class="f-image-cover"></div>
+            </a>
+            <a href="#" id="w-node-d0da5a10-56f6-8d8e-675c-635bd342f160-e6b90512" class="f-gallery-lightbox w-inline-block w-lightbox">
+              <div class="f-gallery-image-s"><img src="../../assets/Placeholder-Gallery.svg" loading="lazy" alt="" class="f-image-cover"></div>
+            </a>
+
+            <a href="#" id="w-node-b3c0b887-9a99-e78a-a47c-b2819b2be34b-e6b90512" class="f-gallery-lightbox w-inline-block w-lightbox">
+              <div class="f-gallery-image-s"><img src="../../assets/Placeholder-Gallery.svg" loading="lazy" alt="" class="f-image-cover"></div>
+            </a>
+
+            <a href="#" id="w-node-a779c83f-e568-e906-8a87-2dae4cda2496-e6b90512" class="f-gallery-lightbox w-inline-block w-lightbox">
+              <div class="f-gallery-image-s"><img src="../../assets/32339.png" loading="lazy" width="Auto" height="Auto" alt="" class="f-image-cover"></div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+
 </style>
