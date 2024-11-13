fda7381b55fb956966bce00069ffeb40644e810e
hana <hana@hanaui-MacBookPro.local>
2024-11-13 22:02:19 +0900

    전윤서 커밋

--- src/components/section/UploadDetailSection.vue
+++ src/components/section/UploadDetailSection.vue
@@ -1,49 +1,156 @@
 <template>
-  <h1></h1>
-  <div class="features-block"></div>
+  <h1>윤서님 페이지 </h1>
   <section class="features-metrics">
     <div class="container-4">
       <div class="pricing-grid">
-        <div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac4f-e6b90512" class="pricing-card-three">
-          <h3>Mobile Device</h3>
+        <div id="w-node-c0e1a6ea-5db7-5b33-b0f3-d2d0229e5be0-9a08dfa8" class="pricing-card-three">
+          <div data-hover="false" data-delay="0" class="f-dropdown-3 w-dropdown">
+            <div class="f-dropdown-toggle-3 w-dropdown-toggle">
+              <div class="f-dropdown-icon-l-3 w-icon-dropdown-toggle"></div>
+              <div class="f-paragraph-small-5">Select</div>
+            </div>
+            <nav class="f-dropdown-list-3 w-dropdown-list">
+              <div class="f-dropdown-wrap-3">
+                <a href="#" class="f-dropdown-link-3 w-inline-block">
+                  <div class="f-dropdown-icon w-embed"><svg width="420" height="420" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M12 2C15.5 2 18 5 19 8C22 9 23 11.73 23 14L20.225 14.793C20.0162 14.8527 19.8325 14.9788 19.7017 15.1522C19.5709 15.3256 19.5001 15.5368 19.5 15.754V17.25C19.5 17.7141 19.3156 18.1592 18.9874 18.4874C18.6592 18.8156 18.2141 19 17.75 19H17.154C16.8236 18.9999 16.4983 19.0818 16.2073 19.2381C15.9162 19.3945 15.6684 19.6205 15.486 19.896C14.558 21.3 13.396 22 12 22C10.604 22 9.442 21.299 8.514 19.896C8.33162 19.6205 8.08382 19.3945 7.79275 19.2381C7.50168 19.0818 7.17641 18.9999 6.846 19H6.25C5.78587 19 5.34075 18.8156 5.01256 18.4874C4.68437 18.1592 4.5 17.7141 4.5 17.25V15.754C4.4999 15.5368 4.4291 15.3256 4.2983 15.1522C4.1675 14.9788 3.98381 14.8527 3.775 14.793L1 14C1 11.734 2 9 5 8C6 5 8.5 2 12 2ZM12 4C9.89 4 7.935 5.788 6.989 8.371L6.897 8.632L6.581 9.582L5.632 9.897C4.377 10.316 3.565 11.238 3.208 12.457L3.185 12.543L4.325 12.87C4.92193 13.0406 5.45122 13.3923 5.83989 13.8764C6.22855 14.3605 6.45744 14.9533 6.495 15.573L6.5 15.754V17H6.846C7.46698 17 8.07943 17.1446 8.63485 17.4223C9.19028 17.7 9.67341 18.1032 10.046 18.6L10.182 18.792C10.758 19.663 11.316 20 12 20C12.638 20 13.167 19.707 13.703 18.96L13.818 18.792C14.1607 18.2743 14.6193 17.8435 15.1573 17.5337C15.6954 17.2238 16.2982 17.0435 16.918 17.007L17.154 17H17.5V15.754C17.4999 15.133 17.6925 14.5272 18.0513 14.0203C18.41 13.5135 18.9173 13.1304 19.503 12.924L19.676 12.87L20.815 12.543L20.792 12.456C20.455 11.305 19.712 10.419 18.572 9.972L18.368 9.897L17.418 9.581L17.103 8.632C16.195 5.91 14.18 4 12 4ZM12 12C12.828 12 13.5 13.12 13.5 14.5C13.5 15.88 12.828 17 12 17C11.172 17 10.5 15.88 10.5 14.5C10.5 13.12 11.172 12 12 12ZM9.5 8C9.89782 8 10.2794 8.15804 10.5607 8.43934C10.842 8.72064 11 9.10218 11 9.5C11 9.89782 10.842 10.2794 10.5607 10.5607C10.2794 10.842 9.89782 11 9.5 11C9.10218 11 8.72064 10.842 8.43934 10.5607C8.15804 10.2794 8 9.89782 8 9.5C8 9.10218 8.15804 8.72064 8.43934 8.43934C8.72064 8.15804 9.10218 8 9.5 8ZM14.5 8C14.8978 8 15.2794 8.15804 15.5607 8.43934C15.842 8.72064 16 9.10218 16 9.5C16 9.89782 15.842 10.2794 15.5607 10.5607C15.2794 10.842 14.8978 11 14.5 11C14.1022 11 13.7206 10.842 13.4393 10.5607C13.158 10.2794 13 9.89782 13 9.5C13 9.10218 13.158 8.72064 13.4393 8.43934C13.7206 8.15804 14.1022 8 14.5 8Z" fill="currentColor"></path>
+                    </svg></div>
+                  <div class="f-paragraph-small-5">Account Settings</div>
+                </a>
+                <a href="#" class="f-dropdown-link-3 w-inline-block">
+                  <div class="f-dropdown-icon w-embed"><svg width="420" height="420" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M11.9999 0.5L16.2259 6.683L23.4129 8.792L18.8379 14.722L19.0529 22.208L11.9999 19.69L4.94691 22.208L5.16191 14.722L0.586914 8.792L7.77391 6.683L11.9999 0.5ZM11.9999 4.044L9.02191 8.402L3.95691 9.887L7.18191 14.065L7.02891 19.34L11.9999 17.566L16.9699 19.34L16.8179 14.065L20.0419 9.887L14.9779 8.402L11.9999 4.044V4.044ZM9.99991 12C9.99991 12.5304 10.2106 13.0391 10.5857 13.4142C10.9608 13.7893 11.4695 14 11.9999 14C12.5303 14 13.0391 13.7893 13.4141 13.4142C13.7892 13.0391 13.9999 12.5304 13.9999 12H15.9999C15.9999 13.0609 15.5785 14.0783 14.8283 14.8284C14.0782 15.5786 13.0608 16 11.9999 16C10.939 16 9.92163 15.5786 9.17149 14.8284C8.42134 14.0783 7.99991 13.0609 7.99991 12H9.99991Z" fill="currentColor"></path>
+                    </svg></div>
+                  <div class="f-paragraph-small-5">Support</div>
+                </a>
+                <a href="#" class="f-dropdown-link-3 w-inline-block">
+                  <div class="f-dropdown-icon w-embed"><svg width="420" height="420" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M17 6H22V8H20V21C20 21.2652 19.8946 21.5196 19.7071 21.7071C19.5196 21.8946 19.2652 22 19 22H5C4.73478 22 4.48043 21.8946 4.29289 21.7071C4.10536 21.5196 4 21.2652 4 21V8H2V6H7V3C7 2.73478 7.10536 2.48043 7.29289 2.29289C7.48043 2.10536 7.73478 2 8 2H16C16.2652 2 16.5196 2.10536 16.7071 2.29289C16.8946 2.48043 17 2.73478 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z" fill="currentColor"></path>
+                    </svg></div>
+                  <div class="f-paragraph-small-5">License</div>
+                </a>
+                <a href="#" class="f-dropdown-link-3 w-inline-block">
+                  <div class="f-dropdown-icon w-embed"><svg width="420" height="420" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M21 1V13C20.9998 14.5736 20.587 16.1197 19.8027 17.484C19.0185 18.8483 17.8902 19.9831 16.5305 20.7752C15.1708 21.5674 13.6272 21.9892 12.0536 21.9986C10.48 22.0079 8.93142 21.6045 7.56238 20.8286C6.19334 20.0527 5.05166 18.9314 4.25124 17.5766C3.45081 16.2217 3.01961 14.6807 3.00065 13.1072C2.98169 11.5336 3.37563 9.98265 4.14318 8.6089C4.91073 7.23516 6.02505 6.08667 7.375 5.278L14 1.453V4.223L21 1ZM19 4.122L12 7.346V4.916L8.597 6.881C7.54678 7.46485 6.66461 8.30931 6.03548 9.33305C5.40634 10.3568 5.05146 11.5253 5.005 12.726L5 13C4.9991 14.8363 5.71983 16.5995 7.0068 17.9093C8.29378 19.2192 10.0439 19.9709 11.88 20.0024C13.716 20.0339 15.4909 19.3426 16.8221 18.0776C18.1532 16.8127 18.9339 15.0753 18.996 13.24L19 13V4.122ZM12 8C13.3261 8 14.5979 8.52678 15.5355 9.46447C16.4732 10.4021 17 11.6739 17 13C17 14.3261 16.4732 15.5979 15.5355 16.5355C14.5979 17.4732 13.3261 18 12 18C10.6739 18 9.40215 17.4732 8.46447 16.5355C7.52678 15.5979 7 14.3261 7 13C7 11.6739 7.52678 10.4021 8.46447 9.46447C9.40215 8.52678 10.6739 8 12 8ZM12 10C11.2044 10 10.4413 10.3161 9.87868 10.8787C9.31607 11.4413 9 12.2044 9 13C9 13.7956 9.31607 14.5587 9.87868 15.1213C10.4413 15.6839 11.2044 16 12 16C12.7956 16 13.5587 15.6839 14.1213 15.1213C14.6839 14.5587 15 13.7956 15 13C15 12.2044 14.6839 11.4413 14.1213 10.8787C13.5587 10.3161 12.7956 10 12 10Z" fill="currentColor"></path>
+                    </svg>
+                  </div>
+                  <div class="f-paragraph-small-5">Sign Out</div>
+                </a>
+              </div>
+            </nav>
+          </div>
           <p class="pricing-card-text">Category<br></p>
         </div>
-        <div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac55-e6b90512" class="pricing-card-three">
-          <h3>Secondary Library</h3>
-          <p class="pricing-card-text">Current Location<br>
-          </p>
+        <div id="w-node-c0e1a6ea-5db7-5b33-b0f3-d2d0229e5bef-9a08dfa8" class="pricing-card-three">
+          <div data-hover="false" data-delay="0" class="f-dropdown-3 w-dropdown">
+            <div class="f-dropdown-toggle-3 w-dropdown-toggle">
+              <div class="f-dropdown-icon-l-3 w-icon-dropdown-toggle"></div>
+              <div class="f-paragraph-small-5">Select</div>
+            </div>
+            <nav class="f-dropdown-list-3 w-dropdown-list">
+              <div class="f-dropdown-wrap-3">
+                <a href="#" class="f-dropdown-link-3 w-inline-block">
+                  <div class="f-dropdown-icon w-embed"><svg width="420" height="420" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M12 2C15.5 2 18 5 19 8C22 9 23 11.73 23 14L20.225 14.793C20.0162 14.8527 19.8325 14.9788 19.7017 15.1522C19.5709 15.3256 19.5001 15.5368 19.5 15.754V17.25C19.5 17.7141 19.3156 18.1592 18.9874 18.4874C18.6592 18.8156 18.2141 19 17.75 19H17.154C16.8236 18.9999 16.4983 19.0818 16.2073 19.2381C15.9162 19.3945 15.6684 19.6205 15.486 19.896C14.558 21.3 13.396 22 12 22C10.604 22 9.442 21.299 8.514 19.896C8.33162 19.6205 8.08382 19.3945 7.79275 19.2381C7.50168 19.0818 7.17641 18.9999 6.846 19H6.25C5.78587 19 5.34075 18.8156 5.01256 18.4874C4.68437 18.1592 4.5 17.7141 4.5 17.25V15.754C4.4999 15.5368 4.4291 15.3256 4.2983 15.1522C4.1675 14.9788 3.98381 14.8527 3.775 14.793L1 14C1 11.734 2 9 5 8C6 5 8.5 2 12 2ZM12 4C9.89 4 7.935 5.788 6.989 8.371L6.897 8.632L6.581 9.582L5.632 9.897C4.377 10.316 3.565 11.238 3.208 12.457L3.185 12.543L4.325 12.87C4.92193 13.0406 5.45122 13.3923 5.83989 13.8764C6.22855 14.3605 6.45744 14.9533 6.495 15.573L6.5 15.754V17H6.846C7.46698 17 8.07943 17.1446 8.63485 17.4223C9.19028 17.7 9.67341 18.1032 10.046 18.6L10.182 18.792C10.758 19.663 11.316 20 12 20C12.638 20 13.167 19.707 13.703 18.96L13.818 18.792C14.1607 18.2743 14.6193 17.8435 15.1573 17.5337C15.6954 17.2238 16.2982 17.0435 16.918 17.007L17.154 17H17.5V15.754C17.4999 15.133 17.6925 14.5272 18.0513 14.0203C18.41 13.5135 18.9173 13.1304 19.503 12.924L19.676 12.87L20.815 12.543L20.792 12.456C20.455 11.305 19.712 10.419 18.572 9.972L18.368 9.897L17.418 9.581L17.103 8.632C16.195 5.91 14.18 4 12 4ZM12 12C12.828 12 13.5 13.12 13.5 14.5C13.5 15.88 12.828 17 12 17C11.172 17 10.5 15.88 10.5 14.5C10.5 13.12 11.172 12 12 12ZM9.5 8C9.89782 8 10.2794 8.15804 10.5607 8.43934C10.842 8.72064 11 9.10218 11 9.5C11 9.89782 10.842 10.2794 10.5607 10.5607C10.2794 10.842 9.89782 11 9.5 11C9.10218 11 8.72064 10.842 8.43934 10.5607C8.15804 10.2794 8 9.89782 8 9.5C8 9.10218 8.15804 8.72064 8.43934 8.43934C8.72064 8.15804 9.10218 8 9.5 8ZM14.5 8C14.8978 8 15.2794 8.15804 15.5607 8.43934C15.842 8.72064 16 9.10218 16 9.5C16 9.89782 15.842 10.2794 15.5607 10.5607C15.2794 10.842 14.8978 11 14.5 11C14.1022 11 13.7206 10.842 13.4393 10.5607C13.158 10.2794 13 9.89782 13 9.5C13 9.10218 13.158 8.72064 13.4393 8.43934C13.7206 8.15804 14.1022 8 14.5 8Z" fill="currentColor"></path>
+                    </svg></div>
+                  <div class="f-paragraph-small-5">Account Settings</div>
+                </a>
+                <a href="#" class="f-dropdown-link-3 w-inline-block">
+                  <div class="f-dropdown-icon w-embed"><svg width="420" height="420" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M11.9999 0.5L16.2259 6.683L23.4129 8.792L18.8379 14.722L19.0529 22.208L11.9999 19.69L4.94691 22.208L5.16191 14.722L0.586914 8.792L7.77391 6.683L11.9999 0.5ZM11.9999 4.044L9.02191 8.402L3.95691 9.887L7.18191 14.065L7.02891 19.34L11.9999 17.566L16.9699 19.34L16.8179 14.065L20.0419 9.887L14.9779 8.402L11.9999 4.044V4.044ZM9.99991 12C9.99991 12.5304 10.2106 13.0391 10.5857 13.4142C10.9608 13.7893 11.4695 14 11.9999 14C12.5303 14 13.0391 13.7893 13.4141 13.4142C13.7892 13.0391 13.9999 12.5304 13.9999 12H15.9999C15.9999 13.0609 15.5785 14.0783 14.8283 14.8284C14.0782 15.5786 13.0608 16 11.9999 16C10.939 16 9.92163 15.5786 9.17149 14.8284C8.42134 14.0783 7.99991 13.0609 7.99991 12H9.99991Z" fill="currentColor"></path>
+                    </svg></div>
+                  <div class="f-paragraph-small-5">Support</div>
+                </a>
+                <a href="#" class="f-dropdown-link-3 w-inline-block">
+                  <div class="f-dropdown-icon w-embed"><svg width="420" height="420" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M17 6H22V8H20V21C20 21.2652 19.8946 21.5196 19.7071 21.7071C19.5196 21.8946 19.2652 22 19 22H5C4.73478 22 4.48043 21.8946 4.29289 21.7071C4.10536 21.5196 4 21.2652 4 21V8H2V6H7V3C7 2.73478 7.10536 2.48043 7.29289 2.29289C7.48043 2.10536 7.73478 2 8 2H16C16.2652 2 16.5196 2.10536 16.7071 2.29289C16.8946 2.48043 17 2.73478 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z" fill="currentColor"></path>
+                    </svg></div>
+                  <div class="f-paragraph-small-5">License</div>
+                </a>
+                <a href="#" class="f-dropdown-link-3 w-inline-block">
+                  <div class="f-dropdown-icon w-embed"><svg width="420" height="420" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M21 1V13C20.9998 14.5736 20.587 16.1197 19.8027 17.484C19.0185 18.8483 17.8902 19.9831 16.5305 20.7752C15.1708 21.5674 13.6272 21.9892 12.0536 21.9986C10.48 22.0079 8.93142 21.6045 7.56238 20.8286C6.19334 20.0527 5.05166 18.9314 4.25124 17.5766C3.45081 16.2217 3.01961 14.6807 3.00065 13.1072C2.98169 11.5336 3.37563 9.98265 4.14318 8.6089C4.91073 7.23516 6.02505 6.08667 7.375 5.278L14 1.453V4.223L21 1ZM19 4.122L12 7.346V4.916L8.597 6.881C7.54678 7.46485 6.66461 8.30931 6.03548 9.33305C5.40634 10.3568 5.05146 11.5253 5.005 12.726L5 13C4.9991 14.8363 5.71983 16.5995 7.0068 17.9093C8.29378 19.2192 10.0439 19.9709 11.88 20.0024C13.716 20.0339 15.4909 19.3426 16.8221 18.0776C18.1532 16.8127 18.9339 15.0753 18.996 13.24L19 13V4.122ZM12 8C13.3261 8 14.5979 8.52678 15.5355 9.46447C16.4732 10.4021 17 11.6739 17 13C17 14.3261 16.4732 15.5979 15.5355 16.5355C14.5979 17.4732 13.3261 18 12 18C10.6739 18 9.40215 17.4732 8.46447 16.5355C7.52678 15.5979 7 14.3261 7 13C7 11.6739 7.52678 10.4021 8.46447 9.46447C9.40215 8.52678 10.6739 8 12 8ZM12 10C11.2044 10 10.4413 10.3161 9.87868 10.8787C9.31607 11.4413 9 12.2044 9 13C9 13.7956 9.31607 14.5587 9.87868 15.1213C10.4413 15.6839 11.2044 16 12 16C12.7956 16 13.5587 15.6839 14.1213 15.1213C14.6839 14.5587 15 13.7956 15 13C15 12.2044 14.6839 11.4413 14.1213 10.8787C13.5587 10.3161 12.7956 10 12 10Z" fill="currentColor"></path>
+                    </svg>
+                  </div>
+                  <div class="f-paragraph-small-5">Sign Out</div>
+                </a>
+              </div>
+            </nav>
+          </div>
+          <p class="pricing-card-text">Current Location<br></p>
         </div>
-        <div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac5b-e6b90512" class="pricing-card-three">
-          <h3>N/A</h3>
+        <div id="w-node-c0e1a6ea-5db7-5b33-b0f3-d2d0229e5bfe-9a08dfa8" class="pricing-card-three">
+          <div class="w-form">
+            <form id="email-form" name="email-form" data-name="Email Form" method="get" data-wf-page-id="67021ce9679c0e259a08dfa8" data-wf-element-id="7a151a48-7e54-5b86-363d-9b3b667168ef">
+              <div class="f-field-wrapper-2"><input class="f-field-input w-input" maxlength="256" name="Input-Field" data-name="Input Field" placeholder="placeholder" type="text" id="Input-Field"></div>
+            </form>
+            <div class="w-form-done">
+              <div>Thank you! Your submission has been received!</div>
+            </div>
+            <div class="w-form-fail">
+              <div>Oops! Something went wrong while submitting the form.</div>
+            </div>
+          </div>
           <p class="pricing-card-text">Name Tag</p>
-        </div><div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac60-e6b90512" class="pricing-card-three">
-          <h3>2/2/2024</h3>
-          <p class="pricing-card-text">Date Found</p>
-        </div><div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac65-e6b90512" class="pricing-card-three">
-          <h3>Found By:&nbsp;Jake</h3>
-          <p class="pricing-card-text">Status &amp;&nbsp;Pickup person name</p>
         </div>
-        <div id="w-node-_85436dbd-cb03-5a38-7065-81d64eb7ac6a-e6b90512" class="pricing-card-three">
-          <h3>2/3/2024</h3>
+        <div id="w-node-_2b8cbf2e-f4c0-3618-4877-b0b84973eb2a-9a08dfa8" class="pricing-card-three">
+          <div class="w-form">
+            <form id="email-form" name="email-form" data-name="Email Form" method="get" data-wf-page-id="67021ce9679c0e259a08dfa8" data-wf-element-id="8bde1b52-3244-3bcb-1e73-6d2037a09d55">
+              <div class="f-field-wrapper-2"><input class="f-field-input w-input" maxlength="256" name="Input-Field-2" data-name="Input Field 2" placeholder="placeholder" type="text" id="Input-Field-2"></div>
+            </form>
+            <div class="w-form-done">
+              <div>Thank you! Your submission has been received!</div>
+            </div>
+            <div class="w-form-fail">
+              <div>Oops! Something went wrong while submitting the form.</div>
+            </div>
+          </div>
+          <p class="pricing-card-text">Date Found</p>
+        </div>
+        <div id="w-node-_9b6af973-a9fb-a055-5a09-fea85fe176c3-9a08dfa8" class="pricing-card-three">
+          <div class="w-form">
+            <form id="email-form" name="email-form" data-name="Email Form" method="get" data-wf-page-id="67021ce9679c0e259a08dfa8" data-wf-element-id="cca6c41c-bd3a-68aa-6f35-e725ea6fdcee">
+              <div class="f-field-wrapper-2"><input class="f-field-input w-input" maxlength="256" name="Input-Field-2" data-name="Input Field 2" placeholder="placeholder" type="text" id="Input-Field-2">
+                <div class="f-toggle-wrap-2">
+                  <div class="f-toggle-regular-2">
+                    <div class="f-toggle-thumb-2"></div>
+                  </div>
+                  <div>Found?</div>
+                </div>
+              </div>
+            </form>
+            <div class="w-form-done">
+              <div>Thank you! Your submission has been received!</div>
+            </div>
+            <div class="w-form-fail">
+              <div>Oops! Something went wrong while submitting the form.</div>
+            </div>
+          </div>
+          <p class="pricing-card-text">Status &amp; Pickup person name</p>
+        </div>
+        <div id="w-node-e9382a67-ffad-90d1-e2e2-fc794f296439-9a08dfa8" class="pricing-card-three">
+          <div class="w-form">
+            <form id="email-form" name="email-form" data-name="Email Form" method="get" data-wf-page-id="67021ce9679c0e259a08dfa8" data-wf-element-id="f88fd881-db45-843c-4a58-324aaba537d9">
+              <div class="f-field-wrapper-2"><input class="f-field-input w-input" maxlength="256" name="Input-Field-2" data-name="Input Field 2" placeholder="placeholder" type="text" id="Input-Field-2"></div>
+            </form>
+            <div class="w-form-done">
+              <div>Thank you! Your submission has been received!</div>
+            </div>
+            <div class="w-form-fail">
+              <div>Oops! Something went wrong while submitting the form.</div>
+            </div>
+          </div>
           <p class="pricing-card-text">Date Retrieved</p>
         </div>
-      </div>
-    </div>
-  </section>
-  <section class="hero-without-image-2">
-    <div class="container-9">
-      <div class="hero-wrapper-two-4">
-        <h1>Description</h1>
-        <p class="margin-bottom-24px-5">Slightly cracked screen, jelly case w/ ornaments inside, out of battery when found</p>
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
+
 </script>
 
-<style scoped>
-
+<style coped>
 
 </style>
